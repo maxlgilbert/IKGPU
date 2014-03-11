2fc8b6eb9218df5cb1113c85524a45a373edb590
Mihail Pivtoraiko <mihail.pivtoraiko@gmail.com>
2014-03-10 18:23:14 -0700

    updated with OpenRAVE installation instructions

---
+++ README.txt
@@ -0,0 +1,9 @@
+This file will contain build instructions.
+
+
+To use the OpenRAVE based visualization, simply install OpenRAVE using the instructions here: http://openrave.org/docs/latest_stable/install/#install
+In Ubuntu, a minimal set of OpenRAVE modules may be installed with:
+$ sudo add-apt-repository ppa:openrave/release
+$ sudo apt-get update
+$ sudo apt-get install openrave-minimal, openrave-python-minimal, openrave0.8-dp-base, openrave0.8-dp-data, openrave0.8-dp-plugin-qtcoinrave, openrave0.8-dp-python
+
---
+++ show_axes.py
@@ -0,0 +1,67 @@
+
+
+def setup_openrave():
+    # this quirky statement is needed for OpenRave 0.8
+    __builtins__.__openravepy_version__ = '0.8'
+   
+    import openravepy as rave
+    or_obj = rave.Environment()
+    or_obj.SetViewer('qtcoin')
+    return or_obj
+
+
+def show_ground_plane(or_obj):
+    ''' 
+    Adds a dark brown flat box representing a ground plane.
+    '''     
+    kinbody_str  = '<KinBody name="ground_plane">'
+    kinbody_str += '  <Body type="static">'
+    kinbody_str += '    <Geom type="box" name="ground_plane_box">'
+    kinbody_str += '      <extents>3 3 0.005</extents>'
+    kinbody_str += '      <diffusecolor>0.5 0.3 0.1</diffusecolor>'
+    kinbody_str += '    </Geom>'
+    kinbody_str += '    <Translation>0 0 -0.005</Translation>'
+    kinbody_str += '  </Body>'
+    kinbody_str += '</KinBody>'
+    kb_ptr = or_obj.ReadKinBodyData(kinbody_str)
+    or_obj.AddKinBody(kb_ptr)
+
+
+
+def spawn_kinbody(or_obj, filename, pose = None):
+    import numpy as np
+
+    kb_ptr = or_obj.ReadKinBodyXMLFile(filename)
+    or_obj.AddKinBody(kb_ptr)
+    if pose is not None:
+        kb_ptr.SetTransform(np.array(pose))
+    return kb_ptr
+
+
+
+if __name__ == '__main__':
+    import sys
+
+    or_obj = setup_openrave()
+    if or_obj is None:
+        print 'Error setting up OpenRave'
+        sys.exit(-1)
+    show_ground_plane(or_obj)
+
+    kb_axes = spawn_kinbody(or_obj, 'data/or_axes.kb.xml')
+
+    kb_smallaxes = spawn_kinbody(or_obj, 'data/or_smallaxes.kb.xml')
+    new_pose = [[0.70710678, -0.70710678, 0, 0], \
+                [0.70710678,  0.70710678, 0, 0], \
+                [0,           0,          1, 0], \
+                [0,           0,          0, 1]]
+    kb_smallaxes.SetTransform(new_pose)
+
+    new_pose = [[1, 0,           0,          0.1], \
+                [0, 0.70710678, -0.70710678, 0.2], \
+                [0, 0.70710678,  0.70710678, 0.3], \
+                [0, 0,           0,          1]]
+    kb_color_axes = spawn_kinbody(or_obj, 'data/color_axes.kb.xml', new_pose)
+
+    raw_input('Press <Enter> when done')
+    or_obj.Destroy()
